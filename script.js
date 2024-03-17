616077a5192a877a78a18f2275839ab09226f4bf
Tushar Paradkar <tusharparadkar2604@gmai.com>
2024-03-17 12:48:59 +0530

    Page 2 animation

--- script.js
+++ script.js
@@ -35,9 +35,66 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 ScrollTrigger.refresh();
 }
 
-gsap.from(".page1-text h1",{
-   y:600,
-   delay:0.5,
-   stagger:0.2,
-   duration:1
+function Page1Animation(){
+  
+  var allText = document.querySelectorAll(".page1-text h1")
+  
+  allText.forEach(function(elem){
+    var clutter = "";
+    var text = elem.textContent
+    var splittedText = text.split("")
+    splittedText.forEach(function(e){
+      clutter+= `<span>${e}</span>`
+    })
+     elem.innerHTML = clutter
+    })
+  
+    gsap.from(".page1-text h1 span",{
+      y:300,
+      delay:0.5,
+      duration:0.6,
+      stagger:0.1,
+      opacity:0,
+    })
+  }
+
+  var cursor = document.querySelector(".cursor")
+
+  window.addEventListener("mousemove",function(dets){
+    // cursor.style.left = dets.x + "px"
+    // cursor.style.top = dets.y + "px"
+    gsap.to(".cursor",{
+      x:dets.x +"px",
+      y:dets.y +"px",
+      duration:0.2
+    })
+  })
+
+var middleText = document.querySelector(".middle-text h1").textContent
+
+var ello = middleText.split(" ")
+var hello = "";
+ello.forEach(function(dets){
+  hello += `<span>${dets}</span>`
+  document.querySelector(".middle-text h1").innerHTML = hello
 })
+
+
+// gsap.from(".middle-text h1 span",{
+//   Y:10,
+//   opacity:0,
+//   stagger:0.1,
+//   duration:0.6,
+//   scrollTrigger:{
+//     trigger:"#page2",
+//     scroller:"#main",
+//     markers:true,
+//     start:"top 53%"
+//   }
+// })
+
+
+locomotive()
+Page1Animation()
+
+
